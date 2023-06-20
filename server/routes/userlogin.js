98d405ea31b3c04aa198e8c163b20e7b69383c0a
bdf007 <bdf007@gmail.com>
2023-06-20 10:54:39 +0200

    authentication for login and logout

--- server/routes/userlogin.js
+++ server/routes/userlogin.js
@@ -2,10 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 // import controllers
-const { register } = require("../controllers/userlogin");
+const {
+  register,
+  login,
+  logout,
+  getLoggedInUser,
+} = require("../controllers/userlogin");
 // import middlewares
-
+const { userRegisterValidator, userById } = require("../middlewares/userlogin");
+const { verifyToken } = require("../middlewares/auth");
 // api routes
-router.post("/register", register);
+router.post("/register", userRegisterValidator, register);
+router.post("/login", login);
+router.get("/logout", logout);
+
+// get logged in user
+router.get("/user", verifyToken, userById, getLoggedInUser);
 
 module.exports = router;
