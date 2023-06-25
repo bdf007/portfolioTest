8a5625119f58e4a191ea1c2cbf8378a50b71b5f6
bdf007 <bdf007@gmail.com>
2023-06-25 09:45:58 +0200

    add the aboutViewer for rendering list of about and modification to readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 # todo
 mettre en forme la partie home 
+ajouter formulaire de contact
+ajouter "laisser un commentaire" visible par tous sauf le mail qui n'est accessible que par moi
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,25 +1,10 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import AboutViewer from "../component/aboutViewer";
 
 const About = () => {
-  const [listOfAbout, setListOfAbout] = useState([]);
-
-  useEffect(() => {
-    axios.get(`${process.env.REACT_APP_API_URL}/about`).then((res) => {
-      setListOfAbout(res.data);
-    });
-  }, [listOfAbout]);
-
   return (
     <div className="container mt-5 mb-5 col-10 col-sm-8 col-md-6 col-lg-5">
-      {listOfAbout.map((about) => {
-        return (
-          <div key={about._id}>
-            <h1>{about.title}</h1>
-            <p>{about.description}</p>
-          </div>
-        );
-      })}
+      <AboutViewer />
     </div>
   );
 };
